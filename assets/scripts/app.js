2bc8653244edfdf42b580307a250c1d220ad3d11
eerie00 <m4mohaiman@gmail.com>
2017-10-06 16:15:36 +0600

    thrid commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,30 +1,32 @@
 "use strict";
 
+
 searchButton.addEventListener('click', searchWeather);
 
-function searchWeather() {
+function searchWeather(){
     loadingText.style.display = 'block';
     weatherBox.style.display = 'none';
     var cityName = searchCity.value;
-    if (cityName.trim().length == 0) {
+    if (cityName.trim().length == 0){
         return alert('Please Enter a City Name');
     }
     var http = new XMLHttpRequest();
     var apiKey = '483781b84a6db74b867dc9648bf9a0fe';
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
+    var  url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
     var method = 'GET';
 
     http.open(method, url);
 
-    http.onreadystatechange = function() {
+    http.onreadystatechange = function (){
 
-        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+        if(http.readyState == XMLHttpRequest.DONE && http.status === 200 ){
             var data = JSON.parse(http.responseText);
             var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
             weatherData.temperature = data.main.temp;
             console.log(weatherData);
             updateWeather(weatherData);
-        } else if (http.readyState == XMLHttpRequest.DONE) {
+        }
+        else if (http.readyState == XMLHttpRequest.DONE){
             alert('something went wrong');
         }
     };
@@ -33,7 +35,7 @@ function searchWeather() {
 
 }
 
-function updateWeather(weatherData) {
+function updateWeather (weatherData){
     weatherCity.textContent = weatherData.cityName;
     weatherDescription.textContent = weatherData.description;
     weatherTemperature.textContent = weatherData.temperature;
